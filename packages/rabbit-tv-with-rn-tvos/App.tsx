d960431b84b39272ba78cc98b0a725752da8bd6c
Pierre Poupin <pierrep@bam.tech>
2023-07-05 15:05:46 +0200

    chore(example): take out TVOS app to separate component

--- packages/rabbit-tv-with-rn-tvos/App.tsx
+++ packages/rabbit-tv-with-rn-tvos/App.tsx
@@ -1,124 +1,12 @@
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  useColorScheme,
-} from 'react-native';
-import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
-
 import {NavigationContainer} from '@react-navigation/native';
-import {Highlight} from './src/components/Highlight';
-import {Program} from './src/components/Program';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {SimpleTVOSApp} from './src/components/SimpleTVOSApp';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <NavigationContainer>
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
-          <Header />
-          <View
-            style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            }}>
-            <Section title="Hightlights">
-              <Highlight />
-              <Highlight />
-              <Highlight />
-            </Section>
-            <Section title="Show of the moment">
-              <Program />
-              <Program />
-              <Program />
-              <Program />
-              <Program />
-              <Program />
-              <Program />
-              <Program />
-              <Program />
-              <Program />
-            </Section>
-            <Section title="Rewatch">
-              <Program />
-              <Program />
-              <Program />
-              <Program />
-            </Section>
-            <Section title="The classics">
-              <Program />
-              <Program />
-              <Program />
-              <Program />
-            </Section>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <SimpleTVOSApp />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
