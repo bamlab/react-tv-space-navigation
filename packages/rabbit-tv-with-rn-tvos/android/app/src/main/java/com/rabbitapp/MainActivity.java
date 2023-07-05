d096f88537fbdad08da1848a82d5c6c964d1f2cb
Pierre Poupin <pierrep@bam.tech>
2023-07-05 17:15:12 +0200

    feat(example): add react native keyevent

--- packages/rabbit-tv-with-rn-tvos/android/app/src/main/java/com/rabbitapp/MainActivity.java
+++ packages/rabbit-tv-with-rn-tvos/android/app/src/main/java/com/rabbitapp/MainActivity.java
@@ -4,9 +4,65 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.github.kevinejohn.keyevent.KeyEventModule;
+
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 public class MainActivity extends ReactActivity {
+  @Override  // <--- Add this method if you want to react to keyDown
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+    // A. Prevent multiple events on long button press
+    //    In the default behavior multiple events are fired if a button
+    //    is pressed for a while. You can prevent this behavior if you
+    //    forward only the first event:
+    //        if (event.getRepeatCount() == 0) {
+    //            KeyEventModule.getInstance().onKeyDownEvent(keyCode, event);
+    //        }
+    //
+    // B. If multiple Events shall be fired when the button is pressed
+    //    for a while use this code:
+    //        KeyEventModule.getInstance().onKeyDownEvent(keyCode, event);
+    //
+    // Using B.
+    KeyEventModule.getInstance().onKeyDownEvent(keyCode, event);
+
+    // There are 2 ways this can be done:
+    //  1.  Override the default keyboard event behavior
+    //    super.onKeyDown(keyCode, event);
+    //    return true;
+
+    //  2.  Keep default keyboard event behavior
+    //    return super.onKeyDown(keyCode, event);
+
+    // Using method #1 without blocking multiple
+    super.onKeyDown(keyCode, event);
+    return true;
+  }
+
+  @Override  // <--- Add this method if you want to react to keyUp
+  public boolean onKeyUp(int keyCode, KeyEvent event) {
+    KeyEventModule.getInstance().onKeyUpEvent(keyCode, event);
+
+    // There are 2 ways this can be done:
+    //  1.  Override the default keyboard event behavior
+    //    super.onKeyUp(keyCode, event);
+    //    return true;
+
+    //  2.  Keep default keyboard event behavior
+    //    return super.onKeyUp(keyCode, event);
+
+    // Using method #1
+    super.onKeyUp(keyCode, event);
+    return true;
+  }
+
+  @Override
+  public boolean onKeyMultiple(int keyCode, int repeatCount, KeyEvent event) {
+    KeyEventModule.getInstance().onKeyMultipleEvent(keyCode, repeatCount, event);
+    return super.onKeyMultiple(keyCode, repeatCount, event);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
