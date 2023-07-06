af8763f71e877916b1fdd347e137eedfb7cd9ca8
Pierre Poupin <pierrep@bam.tech>
2023-07-06 12:05:47 +0200

    chore(example): uniform background for all platforms

--- packages/rabbit-tv-with-rn-tvos/App.tsx
+++ packages/rabbit-tv-with-rn-tvos/App.tsx
@@ -1,10 +1,34 @@
+import {useState, useEffect} from 'react';
+import {View, Dimensions} from 'react-native';
+
+const windowDimensions = Dimensions.get('window');
+
 import {NavigationContainer} from '@react-navigation/native';
 import {SimpleRSNApp} from './src/components/SimpleRSNApp';
 
 function App(): JSX.Element {
+  const [dimensions, setDimensions] = useState({
+    window: windowDimensions,
+  });
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener('change', ({window}) => {
+      setDimensions({window});
+    });
+    return () => subscription?.remove();
+  });
+
   return (
     <NavigationContainer>
-      <SimpleRSNApp />
+      <View
+        // eslint-disable-next-line react-native/no-inline-styles -- dynamic style so inlining is fine
+        style={{
+          width: dimensions.window.width,
+          height: dimensions.window.height,
+          backgroundColor: '#333',
+        }}>
+        <SimpleRSNApp />
+      </View>
     </NavigationContainer>
   );
 }
