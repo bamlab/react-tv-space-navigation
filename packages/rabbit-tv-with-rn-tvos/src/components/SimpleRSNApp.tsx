adea6a3f7901ebbb049cfe8a06ef28d31cc5bf29
Alexandre Colas <alexandr.colas@free.fr>
2023-07-07 14:48:18 +0200

    chore(example): use vertical VirtualizedLists in example

--- packages/rabbit-tv-with-rn-tvos/src/components/SimpleRSNApp.tsx
+++ packages/rabbit-tv-with-rn-tvos/src/components/SimpleRSNApp.tsx
@@ -1,11 +1,14 @@
 import {
   SpatialNavigationRoot,
   SpatialNavigationScrollView,
+  SpatialNavigationView,
 } from 'react-native-tv-spatial-navigation/src';
 
 import './configureKeyboard';
 import {Row} from './VirtualizedRow';
 import {View, StyleSheet} from 'react-native';
+import {Column} from './VirtualizedColumn';
+import {SimpleNode} from './SimpleNode';
 
 export const SimpleRSNApp = () => {
   return (
@@ -21,12 +24,49 @@ export const SimpleRSNApp = () => {
           <View style={styles.spacer} />
           <Row numberOfItems={100} />
         </View>
+        <SpatialNavigationView direction="horizontal">
+          <View style={styles.containerRow}>
+            <View style={styles.spacerRow} />
+            <SpatialNavigationView
+              direction="vertical"
+              style={styles.smallContainerPrograms}>
+              <SimpleNode />
+              <SimpleNode />
+            </SpatialNavigationView>
+            <View style={styles.spacerRow} />
+            <Column numberOfItems={100} />
+            <View style={styles.spacerRow} />
+            <Column numberOfItems={100} />
+            <View style={styles.spacerRow} />
+            <Column numberOfItems={100} />
+          </View>
+        </SpatialNavigationView>
+        <View style={styles.containerPrograms}>
+          <SpatialNavigationView style={styles.gap} direction="horizontal">
+            <SimpleNode />
+            <SimpleNode />
+            <SimpleNode />
+            <SimpleNode />
+          </SpatialNavigationView>
+        </View>
+        <View style={styles.spacer} />
       </SpatialNavigationScrollView>
     </SpatialNavigationRoot>
   );
 };
 
 const styles = StyleSheet.create({
-  spacer: {height: 50},
   container: {padding: 60},
+  spacer: {height: 50},
+  containerRow: {padding: 60, flexDirection: 'row'},
+  spacerRow: {width: 50},
+  smallContainerPrograms: {
+    padding: 70,
+    gap: 50,
+  },
+  containerPrograms: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  gap: {gap: 100},
 });
