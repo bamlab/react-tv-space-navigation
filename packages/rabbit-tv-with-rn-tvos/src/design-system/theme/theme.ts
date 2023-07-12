bd67262b645751e1dc71dfff5e57eeb02c40280f
Yang Liu <yangl@bam.tech>
2023-07-12 16:33:17 +0200

    feat: add Typography

--- packages/rabbit-tv-with-rn-tvos/src/design-system/theme/theme.ts
+++ packages/rabbit-tv-with-rn-tvos/src/design-system/theme/theme.ts
@@ -1,5 +1,6 @@
 import { colors } from './colors';
 import { sizes } from './sizes';
 import { spacings } from './spacings';
+import { typography } from './typography';
 
-export const theme = { spacings, sizes, colors };
+export const theme = { spacings, sizes, colors, typography };
