aed59d39cc4727c7192edb9016e760ba689144a1
Yang Liu <yangl@bam.tech>
2023-07-19 16:03:11 +0200

    refactor: remove unused helper function

--- packages/rabbit-tv-with-rn-tvos/src/components/atom/Program/infra/getProgramById.ts
+++
@@ -1,9 +0,0 @@
-import { programs } from './programs';
-
-export const getProgramById = (id: string) => {
-  const program = programs.find((program) => program.id === id);
-  if (!program) {
-    console.warn(`Program with id ${id} not found, please check the id`);
-  }
-  return program;
-};
