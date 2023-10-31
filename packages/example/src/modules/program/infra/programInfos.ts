7cbdb4eac08c58207a184978fc8f5d307720d87a
Pierre Poupin <pierrep@bam.tech>
2023-10-31 17:47:27 +0100

    chore(example): add more bunny movies

--- packages/example/src/modules/program/infra/programInfos.ts
+++ packages/example/src/modules/program/infra/programInfos.ts
@@ -8,6 +8,22 @@ const Rabbit6 = require('../assets/rabbit6.png');
 const Rabbit7 = require('../assets/rabbit7.png');
 const Rabbit8 = require('../assets/rabbit8.png');
 const Rabbit9 = require('../assets/rabbit9.png');
+const Rabbit10 = require('../assets/rabbit10.png');
+const Rabbit11 = require('../assets/rabbit11.png');
+const Rabbit12 = require('../assets/rabbit12.png');
+const Rabbit13 = require('../assets/rabbit13.png');
+const Rabbit14 = require('../assets/rabbit14.png');
+const Rabbit15 = require('../assets/rabbit15.png');
+const Rabbit16 = require('../assets/rabbit16.png');
+const Rabbit17 = require('../assets/rabbit17.png');
+const Rabbit18 = require('../assets/rabbit18.png');
+const Rabbit19 = require('../assets/rabbit19.png');
+const Rabbit20 = require('../assets/rabbit20.png');
+const Rabbit21 = require('../assets/rabbit21.png');
+const Rabbit22 = require('../assets/rabbit22.png');
+const Rabbit23 = require('../assets/rabbit23.png');
+const Rabbit24 = require('../assets/rabbit24.png');
+const Rabbit25 = require('../assets/rabbit25.png');
 import { ProgramInfo } from '../domain/programInfo';
 
 export const programInfos: ProgramInfo[] = [
@@ -64,5 +80,101 @@ export const programInfos: ProgramInfo[] = [
     title: 'Program 9',
     image: Rabbit9,
     description: 'Program 9 description',
+  },
+  {
+    id: '10',
+    title: 'Program 10',
+    image: Rabbit10,
+    description: 'Program 10 description',
+  },
+  {
+    id: '11',
+    title: 'Program 11',
+    image: Rabbit11,
+    description: 'Program 11 description',
+  },
+  {
+    id: '12',
+    title: 'Program 12',
+    image: Rabbit12,
+    description: 'Program 12 description',
+  },
+  {
+    id: '13',
+    title: 'Program 13',
+    image: Rabbit13,
+    description: 'Program 13 description',
+  },
+  {
+    id: '14',
+    title: 'Program 14',
+    image: Rabbit14,
+    description: 'Program 14 description',
+  },
+  {
+    id: '15',
+    title: 'Program 15',
+    image: Rabbit15,
+    description: 'Program 15 description',
+  },
+  {
+    id: '16',
+    title: 'Program 16',
+    image: Rabbit16,
+    description: 'Program 16 description',
+  },
+  {
+    id: '17',
+    title: 'Program 17',
+    image: Rabbit17,
+    description: 'Program 17 description',
+  },
+  {
+    id: '18',
+    title: 'Program 18',
+    image: Rabbit18,
+    description: 'Program 18 description',
+  },
+  {
+    id: '19',
+    title: 'Program 19',
+    image: Rabbit19,
+    description: 'Program 19 description',
+  },
+  {
+    id: '20',
+    title: 'Program 20',
+    image: Rabbit20,
+    description: 'Program 20 description',
+  },
+  {
+    id: '21',
+    title: 'Program 21',
+    image: Rabbit21,
+    description: 'Program 21 description',
+  },
+  {
+    id: '22',
+    title: 'Program 22',
+    image: Rabbit22,
+    description: 'Program 22 description',
+  },
+  {
+    id: '23',
+    title: 'Program 23',
+    image: Rabbit23,
+    description: 'Program 23 description',
+  },
+  {
+    id: '24',
+    title: 'Program 24',
+    image: Rabbit24,
+    description: 'Program 24 description',
+  },
+  {
+    id: '25',
+    title: 'Program 25',
+    image: Rabbit25,
+    description: 'Program 25 description',
   },
 ];
