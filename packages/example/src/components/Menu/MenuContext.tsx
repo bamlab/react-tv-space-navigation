1c8dfa6e56063222fadc3d68fbd24c704f99d210
Pierre Poupin <pierrep@bam.tech>
2023-11-01 17:55:36 +0100

    chore(example): add menu implementation example

---
+++ packages/example/src/components/Menu/MenuContext.tsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, useMemo, useState } from 'react';
+
+const MenuContext = createContext<{ isOpen: boolean; toggleMenu: (isOpen: boolean) => void }>({
+  isOpen: false,
+  toggleMenu: () => {
+    throw new Error('MenuContext not initialized');
+  },
+});
+
+export const MenuProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const value = useMemo(() => {
+    return { isOpen, toggleMenu: setIsOpen };
+  }, [isOpen, setIsOpen]);
+
+  return <MenuContext.Provider value={value}>{children}</MenuContext.Provider>;
+};
+
+export const useMenuContext = () => useContext(MenuContext);
--- packages/example/src/components/Page.tsx
+++ packages/example/src/components/Page.tsx
@@ -1,10 +1,32 @@
+import { Direction } from '@bam.tech/lrud';
 import { useIsFocused } from '@react-navigation/native';
-import { ReactNode } from 'react';
+import { ReactNode, useCallback } from 'react';
 import { SpatialNavigationRoot } from 'react-tv-space-navigation';
+import { useMenuContext } from './Menu/MenuContext';
 
 type Props = { children: ReactNode };
 
 export const Page = ({ children }: Props) => {
   const isFocused = useIsFocused();
-  return <SpatialNavigationRoot isActive={isFocused}>{children}</SpatialNavigationRoot>;
+  const { isOpen: isMenuOpen, toggleMenu } = useMenuContext();
+
+  const isActive = isFocused && !isMenuOpen;
+
+  const onDirectionHandledWithoutMovement = useCallback(
+    (movement: Direction) => {
+      if (movement === 'left') {
+        toggleMenu(true);
+      }
+    },
+    [toggleMenu],
+  );
+
+  return (
+    <SpatialNavigationRoot
+      isActive={isActive}
+      onDirectionHandledWithoutMovement={onDirectionHandledWithoutMovement}
+    >
+      {children}
+    </SpatialNavigationRoot>
+  );
 };
