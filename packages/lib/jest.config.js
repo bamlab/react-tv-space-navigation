0ccbb5090c6029ba648aa68b79caceb3184973a1
JulienIzz <julien.izzillo@gmail.com>
2024-02-01 14:21:36 +0100

    chore: fixing jest file config to an absolute path

--- packages/lib/jest.config.js
+++ packages/lib/jest.config.js
@@ -7,6 +7,10 @@
  *
  * If encountering a syntax error during tests with a new package, add it to this list
  */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
 const packagesToTransform = [
   'react-native',
   'react-native-(.*)',
@@ -30,7 +34,10 @@ const config = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testRegex: '\\.test\\.[jt]sx?$',
   transform: {
-    '\\.[jt]sx?$': ['babel-jest', { configFile: './babel.jest.config.js' }],
+    '\\.[jt]sx?$': [
+      'babel-jest',
+      { configFile: path.resolve(__dirname, './babel.jest.config.js') },
+    ],
   },
   transformIgnorePatterns: [`node_modules/(?!(${packagesToTransform.join('|')})/)`],
   cacheDirectory: '.cache/jest',
