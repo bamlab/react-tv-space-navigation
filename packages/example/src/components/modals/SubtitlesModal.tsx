b60ebc192c9e6a039d73e879db0b7a6bc72b5714
Thomas Renaud <thomasre@bam.tech>
2024-02-06 16:19:35 +0100

    Add subtitles modal in program details page

---
+++ packages/example/src/components/modals/SubtitlesModal.tsx
@@ -0,0 +1,57 @@
+import { SpatialNavigationRoot } from '../../../../lib/src/spatial-navigation/components/Root';
+import { DefaultFocus } from '../../../../lib/src/spatial-navigation/context/DefaultFocusContext';
+import { Button } from '../../design-system/components/Button';
+import { Spacer } from '../../design-system/components/Spacer';
+import { GenericModal } from './GenericModal';
+
+interface SubtitlesModalProps {
+  isModalVisible: boolean;
+  setIsModalVisible: (isVisible: boolean) => void;
+  setSubtitles: (subtitles: string) => void;
+}
+
+export const SubtitlesModal = ({
+  isModalVisible,
+  setIsModalVisible,
+  setSubtitles,
+}: SubtitlesModalProps) => {
+  return (
+    <SpatialNavigationRoot>
+      <GenericModal isVisible={isModalVisible} title={'Choose subtitles'}>
+        <DefaultFocus>
+          <Button
+            label="English"
+            onSelect={() => {
+              setSubtitles('English');
+              setIsModalVisible(false);
+            }}
+          />
+        </DefaultFocus>
+        <Spacer gap="$8" />
+        <Button
+          label="Spanish"
+          onSelect={() => {
+            setSubtitles('Spanish');
+            setIsModalVisible(false);
+          }}
+        />
+        <Spacer gap="$8" />
+        <Button
+          label="Portuguese"
+          onSelect={() => {
+            setSubtitles('Portuguese');
+            setIsModalVisible(false);
+          }}
+        />
+        <Spacer gap="$8" />
+        <Button
+          label="None"
+          onSelect={() => {
+            setSubtitles('No');
+            setIsModalVisible(false);
+          }}
+        />
+      </GenericModal>
+    </SpatialNavigationRoot>
+  );
+};
--- packages/example/src/pages/ProgramDetail.tsx
+++ packages/example/src/pages/ProgramDetail.tsx
@@ -9,7 +9,7 @@ import { Typography } from '../design-system/components/Typography';
 import { ProgramListWithTitle } from '../modules/program/view/ProgramListWithTitle';
 import { Button } from '../design-system/components/Button';
 import { useState } from 'react';
-import { GenericModal } from '../components/GenericModal';
+import { SubtitlesModal } from '../components/modals/SubtitlesModal';
 
 export const ProgramDetail = ({
   route,
@@ -17,47 +17,47 @@ export const ProgramDetail = ({
   route: RouteProp<RootStackParamList, 'ProgramDetail'>;
 }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [subtitles, setSubtitles] = useState('No');
   const { programInfo } = route.params;
 
   return (
-    <Page>
-      <Box padding="$5">
-        <Container paddingHorizontal="$15" paddingTop="$10" direction="horizontal">
-          <JumbotronContainer>
-            <Jumbotron source={programInfo.image} />
-          </JumbotronContainer>
-          <DefaultFocus>
-            <Box padding="$15" flex={1}>
-              <Typography variant="title" fontWeight="strong">
-                {programInfo.title}
-              </Typography>
-              <Spacer gap="$15" />
-              <Description variant="body" fontWeight="strong">
-                {programInfo.description}
-              </Description>
-              <Spacer gap="$8" />
-              {/* eslint-disable-next-line no-console */}
-              <Button label="Play" onSelect={() => console.log('Playing!')} />
-              <Spacer gap="$8" />
-              {/* eslint-disable-next-line no-console */}
-              <Button label="More info" onSelect={() => console.log('More info!')} />
-              <Spacer gap="$8" />
-              {/* eslint-disable-next-line no-console */}
-              <Button label="Choose subtitles" onSelect={() => setIsModalVisible(true)} />
-              <GenericModal isVisible={isModalVisible}>
-                <Button label="English" onSelect={() => setIsModalVisible(false)} />
-                <Spacer direction="horizontal" gap="$8" />
-                <Button label="Spanish" onSelect={() => setIsModalVisible(false)} />
-                <Spacer direction="horizontal" gap="$8" />
-                <Button label="French" onSelect={() => setIsModalVisible(false)} />
-              </GenericModal>
-            </Box>
-          </DefaultFocus>
-        </Container>
-        <Spacer gap="$5" />
-        <ProgramListWithTitle title="You may also like..."></ProgramListWithTitle>
-      </Box>
-    </Page>
+    <>
+      <Page isPageActive={!isModalVisible}>
+        <Box padding="$5">
+          <Container paddingHorizontal="$15" paddingTop="$10" direction="horizontal">
+            <JumbotronContainer>
+              <Jumbotron source={programInfo.image} />
+            </JumbotronContainer>
+            <DefaultFocus>
+              <Box padding="$15" flex={1}>
+                <Typography variant="title" fontWeight="strong">
+                  {programInfo.title}
+                </Typography>
+                <Spacer gap="$15" />
+                <Description variant="body" fontWeight="strong">
+                  {programInfo.description}
+                </Description>
+                <Spacer gap="$8" />
+                {/* eslint-disable-next-line no-console */}
+                <Button label="Play" onSelect={() => console.log('Playing!')} />
+                <Spacer gap="$8" />
+                {/* eslint-disable-next-line no-console */}
+                <Button label="More info" onSelect={() => console.log('More info!')} />
+                <Spacer gap="$8" />
+                <Button label={`${subtitles} subtitles`} onSelect={() => setIsModalVisible(true)} />
+              </Box>
+            </DefaultFocus>
+          </Container>
+          <Spacer gap="$5" />
+          <ProgramListWithTitle title="You may also like..."></ProgramListWithTitle>
+        </Box>
+      </Page>
+      <SubtitlesModal
+        isModalVisible={isModalVisible}
+        setIsModalVisible={setIsModalVisible}
+        setSubtitles={setSubtitles}
+      />
+    </>
   );
 };
 
