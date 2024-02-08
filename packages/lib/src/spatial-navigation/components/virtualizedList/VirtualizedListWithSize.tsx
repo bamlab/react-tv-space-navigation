4c9a141768b77237a8cb39e49fa0e9a2044092eb
JulienIzz <julien.izzillo@gmail.com>
2024-02-08 11:46:21 +0100

    docs(lists): add TSDoc with drawings

--- packages/lib/src/spatial-navigation/components/virtualizedList/VirtualizedListWithSize.tsx
+++ packages/lib/src/spatial-navigation/components/virtualizedList/VirtualizedListWithSize.tsx
@@ -7,6 +7,8 @@ import { useState } from 'react';
  * This component has for only purpose to give to the VirtualizedList its actual
  * width and height. It is used to avoid the VirtualizedList to render with a width
  * or height not defined (as it is used later for computing offsets for example).
+ * The size is computed only once and then the VirtualizedList is rendered. This
+ * doesn't support dynamic size changes.
  */
 export const VirtualizedListWithSize = typedMemo(
   <T extends ItemWithIndex>(props: Omit<VirtualizedListProps<T>, 'width' | 'height'>) => {
--- packages/lib/src/spatial-navigation/components/virtualizedList/helpers/getLastItemIndex.ts
+++ packages/lib/src/spatial-navigation/components/virtualizedList/helpers/getLastItemIndex.ts
@@ -1,3 +1,27 @@
+/**
+ * This function is used to compute the index of the last item that allows the end of the list to fully fit in the screen.
+ * It is used when scrolling on stick-to-start mode.
+ *
+ * ```
+ *      ┌───────────────────────────────────────┐
+ *      │                Screen                 │
+ *      │                                       │
+ *      │                                       │
+ *  ┌───┼─┐  ┌─────┐  ┌─────┐  ┌─────┐  ┌─────┐ │
+ *  │  3│ │  │  4  │  │  5  │  │  6  │  │  7  │ │
+ *  │┌──┼┐│  │┌───┐│  │┌───┐│  │┌───┐│  │┌───┐│ │
+ *  ││ C│││  ││ D ││  ││ E ││  ││ F ││  ││ G ││ │
+ *  │└──┼┘│  │└───┘│  │└───┘│  │└───┘│  │└───┘│ │
+ *  └───┼─┘  └─────┘  └─────┘  └─────┘  └─────┘ │
+ *      │                                       │
+ *      └───────────────────────────────────────┘
+ * ```
+ *
+ * In this case the last item that allows the end of the list to fully fit in the screen is item 4, so the
+ * scroll in stick-to-start mode will be stopped after item 4, to keep
+ * item 4 to 7 in the screen.
+ *
+ */
 export const getLastLeftItemIndex = <T>(
   data: T[],
   itemSizeInPx: number | ((item: T) => number),
@@ -20,6 +44,31 @@ export const getLastLeftItemIndex = <T>(
   return data.length - Math.floor(listSizeInPx / itemSizeInPx);
 };
 
+/**
+ *
+ * This function is used to compute the index of the last item that fits in the screen when at the beginning of a list.
+ * It is used when scrolling on stick-to-end mode to know when to start or stop scrolling
+ *
+ * ```
+ *  ┌───────────────────────────────────────┐
+ *  │                Screen                 │
+ *  │                                       │
+ *  │                                       │
+ *  │ ┌─────┐  ┌─────┐  ┌─────┐  ┌─────┐  ┌─┼───┐
+ *  │ │  1  │  │  2  │  │  3  │  │  4  │  │ │5  │
+ *  │ │┌───┐│  │┌───┐│  │┌───┐│  │┌───┐│  │┌┼──┐│
+ *  │ ││ A ││  ││ B ││  ││ C ││  ││ D ││  │││E ││
+ *  │ │└───┘│  │└───┘│  │└───┘│  │└───┘│  │└┼──┘│
+ *  │ └─────┘  └─────┘  └─────┘  └─────┘  └─┼───┘
+ *  │                                       │
+ *  └───────────────────────────────────────┘
+ * ```
+ *
+ * In this case the last item that fits in the screen is item 4, so the
+ * scroll in stick-to-end mode will be computed after item 4, to keep
+ * item 5 (and the followings) on the right of the screen.
+ *
+ */
 export const getLastRightItemIndex = <T>(
   data: T[],
   itemSizeInPx: number | ((item: T) => number),
