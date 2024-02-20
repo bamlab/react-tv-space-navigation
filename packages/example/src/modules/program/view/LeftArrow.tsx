dd8b145afd8a31d0d73818edb1ee248143d1ea66
JulienIzz <julien.izzillo@gmail.com>
2024-02-20 18:01:50 +0100

    chore: add arrows in example app

--- packages/example/src/modules/program/view/LeftArrow.tsx
+++ packages/example/src/modules/program/view/LeftArrow.tsx
@@ -1,59 +1,76 @@
 import styled from '@emotion/native';
-import { Dimensions, View } from 'react-native';
-import { colors } from '../../../design-system/theme/colors';
-
-export const LeftArrow = () => {
-  return <StyledLeftView />;
-};
+import { Dimensions, View, Image } from 'react-native';
 
 const windowDimensions = Dimensions.get('window');
 
-const StyledLeftView = styled(View)({
-  width: 200,
+const StyledScrollZone = styled(View)({
+  width: 120,
   height: 400,
-  opacity: 0.2,
   position: 'absolute',
-  backgroundColor: colors.background.light,
-  transform: [{ translateX: -100 }, { translateY: -50 }],
+  top: -98,
+  justifyContent: 'center',
+  alignItems: 'center',
 });
+
+const StyledLeftView = styled(StyledScrollZone)({
+  left: -62,
+});
+
+const StyledRightView = styled(StyledScrollZone)({
+  right: -62,
+});
+
+const LeftArrowImage = styled(Image)({
+  height: 70,
+  width: 50,
+  resizeMode: 'stretch',
+  transform: [{ rotate: '180deg' }],
+  tintColor: 'white',
+});
+
+const RightArrowImage = styled(Image)({
+  height: 70,
+  width: 50,
+  resizeMode: 'stretch',
+  tintColor: 'white',
+});
+
+export const LeftArrow = () => {
+  return (
+    <StyledLeftView>
+      <LeftArrowImage source={require('../assets/arrow-left.png')} />
+    </StyledLeftView>
+  );
+};
 
 export const RihtArrow = () => {
-  return <StyledRightView />;
+  return (
+    <StyledRightView>
+      <RightArrowImage source={require('../assets/arrow-left.png')} />
+    </StyledRightView>
+  );
 };
 
-const StyledRightView = styled(View)({
-  width: 200,
-  height: 400,
-  opacity: 0.2,
-  position: 'absolute',
-  backgroundColor: colors.background.light,
-  transform: [{ translateX: 100 }, { translateY: -50 }],
-  top: 0,
-  right: 0,
-});
-
-export const TopArrow = () => {
-  return <StyledTopView />;
-};
-
-const StyledTopView = styled(View)({
+const StyledVerticalScrollZone = styled(View)({
   width: windowDimensions.width,
   height: 120,
   opacity: 0.2,
   position: 'absolute',
   backgroundColor: 'red',
+});
+
+const StyledTopView = styled(StyledVerticalScrollZone)({
   top: 0,
+});
+
+const StyledBottomView = styled(StyledVerticalScrollZone)({
+  bottom: 0,
 });
 
 export const BottomArrow = () => {
   return <StyledBottomView />;
 };
 
-const StyledBottomView = styled(View)({
-  width: windowDimensions.width,
-  height: 120,
-  opacity: 0.2,
-  position: 'absolute',
-  backgroundColor: 'red',
-  bottom: 0,
-});
+export const TopArrow = () => {
+  return <StyledTopView />;
+};
