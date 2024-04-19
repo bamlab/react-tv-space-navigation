f9824b5c4d4eb19dd9aeea6c0c78ee3c0ee70b63
Pierre Poupin <pierrep@bam.tech>
2024-04-19 17:56:30 +0200

    doc: add tsdoc

--- packages/lib/src/spatial-navigation/types/SpatialNavigationNodeRef.ts
+++ packages/lib/src/spatial-navigation/types/SpatialNavigationNodeRef.ts
@@ -1,4 +1,11 @@
 export type SpatialNavigationNodeRef = {
+  /**
+   * Imperatively grab the focus on the node.
+   */
   focus: () => void;
+  /**
+   * Scroll to a given node, without grabbing the focus.
+   * https://github.com/bamlab/react-tv-space-navigation/issues/106
+   */
   triggerScroll: () => void;
 };
