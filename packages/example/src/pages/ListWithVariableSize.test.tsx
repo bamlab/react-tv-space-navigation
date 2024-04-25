00b0c80cc144d5f5c50d0dfd782d1b32f6cf1f73
Rémi Leroy <remil@bam.tech>
2024-04-25 15:04:25 +0200

    test: add test for list with variable size

---
+++ packages/example/src/pages/ListWithVariableSize.test.tsx
@@ -0,0 +1,49 @@
+import { ThemeProvider } from '@emotion/react';
+import { render } from '@testing-library/react-native';
+import { theme } from '../design-system/theme/theme';
+import { ListWithVariableSize } from './ListWithVariableSize';
+import { NavigationContainer } from '@react-navigation/native';
+import '../components/tests/helpers/configureTestRemoteControl';
+import testRemoteControlManager from '../components/tests/helpers/testRemoteControlManager';
+
+const renderWithProviders = (page: JSX.Element) => {
+  return render(
+    <NavigationContainer>
+      <ThemeProvider theme={theme}>{page}</ThemeProvider>
+    </NavigationContainer>,
+  );
+};
+describe('ListWithVariableSize', () => {
+  it('node is still focusable after being removed', () => {
+    const screen = renderWithProviders(<ListWithVariableSize />);
+
+    // Go to last programd and focus it
+    testRemoteControlManager.handleRight();
+    testRemoteControlManager.handleRight();
+    testRemoteControlManager.handleRight();
+    expect(screen.getByLabelText('Program 4')).toBeSelected();
+
+    // Go back to first position
+    testRemoteControlManager.handleLeft();
+    testRemoteControlManager.handleLeft();
+    testRemoteControlManager.handleLeft();
+
+    // Remove last item
+    testRemoteControlManager.handleDown();
+    testRemoteControlManager.handleEnter();
+
+    expect(screen.queryByLabelText('Program 4')).not.toBeOnTheScreen();
+
+    // Add back last item
+    testRemoteControlManager.handleEnter();
+    expect(screen.getByLabelText('Program 4')).toBeOnTheScreen();
+
+    // Focus last item
+    testRemoteControlManager.handleUp();
+    testRemoteControlManager.handleRight();
+    testRemoteControlManager.handleRight();
+    testRemoteControlManager.handleRight();
+
+    expect(screen.getByLabelText('Program 4')).toBeSelected();
+  });
+});
