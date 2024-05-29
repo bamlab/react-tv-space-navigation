e89d0efd6436b02b799922e0fd6e573e8ddb5385
Alexandre Colas <alexandr.colas@free.fr>
2024-05-29 12:28:09 +0200

    refactor(Lists): delete addIndex helper

--- packages/lib/src/spatial-navigation/components/virtualizedList/helpers/addIndex.ts
+++
@@ -1,5 +0,0 @@
-import { ItemWithIndex } from '../VirtualizedList';
-
-export const addIndex = <T>(array: Array<T>): Array<T & ItemWithIndex> => {
-  return array.map((value, index) => ({ index, ...value }));
-};
